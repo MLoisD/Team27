1121ef2b1a1bad97dd8aaaefd941272b37e9737e
MLoisD <davidssinmi@gmail.com>
2023-02-16 15:31:10 +0000

    spring file made

---
+++ backend/inventoryb/src/main/java/com/t27/inventoryb/InventorybApplication.java
@@ -0,0 +1,13 @@
+package com.t27.inventoryb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorybApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorybApplication.class, args);
+	}
+
+}
---
+++ backend/inventoryb/src/test/java/com/t27/inventoryb/InventorybApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t27.inventoryb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorybApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
