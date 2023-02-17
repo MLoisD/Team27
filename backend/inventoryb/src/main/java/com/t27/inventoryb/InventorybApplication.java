36e6819009b3975c46d7962c946c79ab272fa6bc
MLoisD <davidssinmi@gmail.com>
2023-02-17 13:17:48 +0000

    launching

--- backend/inventoryb/src/main/java/com/t27/inventoryb/InventorybApplication.java
+++ backend/inventoryb/src/main/java/com/t27/inventoryb/InventorybApplication.java
@@ -2,8 +2,10 @@ package com.t27.inventoryb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class InventorybApplication {
 
 	public static void main(String[] args) {
