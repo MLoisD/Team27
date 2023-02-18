3798e281aab806e57e20e8882aa78910b30cd701
MLoisD <davidssinmi@gmail.com>
2023-02-18 17:31:26 +0000

    Updating react code

--- backend/inventoryb/src/main/java/com/t27/inventoryapp/controller/CustomerController.java
+++ backend/inventoryb/src/main/java/com/t27/inventoryapp/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.t27.inventoryapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import com.t27.inventoryapp.model.Customer;
 import java.util.*;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class CustomerController{
 
     @Autowired
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,28 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
-  return (
-    <div className='container'>
-        <div className='py'>
-        <table className="table">
-  <thead>
-    <tr>
-      <th scope="col">id</th>
-      <th scope="col">username</th>
-      <th scope="col">email</th>
-      <th scope="col">type</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</table>
+
+    const [customer, setCustomers] = useState([])
+
+    useEffect(() => {
+        loadCustomers();
+    }, []);
+
+    const loadCustomers = async () => {
+        const result = await axios.get("http://localhost:8080/customerList")
+        setCustomers(result.data)
+    }
+
+    return (
+        <div className='container'>
+            <div className='py'>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">id</th>
+                            <th scope="col">username</th>
+                            <th scope="col">email</th>
+                            <th scope="col">type</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            customer.map((customer, index) => (
+                                <tr>
+                                    <th scope="row" key={index}>{index +1}</th>
+                                    <td>{customer.uname}</td>
+                                    <td>{customer.uemail}</td>
+                                    <td>{customer.userType}</td>
+                                    <td>
+                                        <button className="btn btn-outline-danger mx-2">View Order</button>
+                                        <button className="btn btn-primary mx-2">Edit User</button>
+                                        <button className="btn btn-danger mx-2">Delete User</button>
+                                    </td>
+                                </tr>
+                            ))}
+
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
