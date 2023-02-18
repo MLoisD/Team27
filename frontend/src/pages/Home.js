1dbc3cd3a49e9a5a650bc2e0f0d2389a56f4153e
MLoisD <davidssinmi@gmail.com>
2023-02-18 16:21:27 +0000

    editing react 2/?

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='container'>
+        <div className='py'>
+        <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">username</th>
+      <th scope="col">email</th>
+      <th scope="col">type</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    </div>
+  )
+}
