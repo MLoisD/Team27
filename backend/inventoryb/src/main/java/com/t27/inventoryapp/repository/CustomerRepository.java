e1dc7345ee5154cef656d69967e581b24f165570
MLoisD <davidssinmi@gmail.com>
2023-02-21 15:22:14 +0000

    new branch

--- backend/inventoryb/src/main/java/com/t27/inventoryapp/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.t27.inventoryapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.t27.inventoryapp.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
-    
-}  
