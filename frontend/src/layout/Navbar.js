c1b3f70450c62575a5a0ac140ffdd12c9905d1ac
MLoisD <davidssinmi@gmail.com>
2023-02-25 15:08:51 +0000

    adding books to the database

--- frontend/src/layout/Navbar.js
+++ frontend/src/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
@@ -10,7 +11,7 @@ export default function Navbar() {
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <button className='btn btn-outline-light'>Add User</button>
+                    <Link className='btn btn-outline-light' to="/addBook">Add Book</Link>
                 </div>
             </nav>
         </div>
