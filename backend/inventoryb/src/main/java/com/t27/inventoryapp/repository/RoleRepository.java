894e067f2bfc4bdf5a928609efe43eaa28d1168e
MLoisD <davidssinmi@gmail.com>
2023-03-05 17:03:40 +0000

    edits on security implementation

--- backend/inventoryb/src/main/java/com/t27/inventoryapp/repository/RoleRepository.java
+++ backend/inventoryb/src/main/java/com/t27/inventoryapp/repository/RoleRepository.java
@@ -3,11 +3,10 @@ package com.t27.inventoryapp.repository;
 import java.util.Optional; 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.t27.inventoryapp.model.*;
 
-@Repository
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
 	Optional<Role> findByName(ERole name);
 }
--- backend/inventoryb/src/main/java/com/t27/inventoryapp/security/WebSecurityConfig.java
+++ backend/inventoryb/src/main/java/com/t27/inventoryapp/security/WebSecurityConfig.java
@@ -1,23 +1,35 @@
 package com.t27.inventoryapp.security;
 
-import com.t27.inventoryapp.security.jwt.AuthTokenFilter;
 import com.t27.inventoryapp.security.services.*;
 import com.t27.inventoryapp.security.jwt.*;
+
+import com.t27.inventoryapp.security.jwt.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
-@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@EnableMethodSecurity(//prePostEnabled = true, //securedEnabled = true, 
+jsr250Enabled = true)
 public class WebSecurityConfig {
 
     @Autowired
-     UserDetailsServiceImpl userDetailService;
-
+    UserDetailsService userDetailsService;
+  
     @Autowired
-    private AuthEntryPointJwt unathorizedHandler;
+    private AuthEntryPointJwt unauthorizedHandler;
 
     @Bean
     public AuthTokenFilter authenJwtTokenFilter() {
@@ -25,12 +37,38 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public DaoAuthenticationProvider authenProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenProvider = new DaoAuthenticationProvider();
 
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-     
-        return authProvider;
+        authenProvider.setUserDetailsService(userDetailsService);
+        authenProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenConfig) throws Exception {
+        return authenConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authoriseRequests().antMatchers("/t27/auth/**").permitAll()
+                .antMatchers("/t27/test/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.authenticationProvider(authenticationProvider());
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
