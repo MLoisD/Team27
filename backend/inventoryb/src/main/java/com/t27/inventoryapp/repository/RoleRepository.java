003ab5c672183e9222134bae158a1e4e51c134ed
MLoisD <davidssinmi@gmail.com>
2023-03-05 15:42:07 +0000

    securtity

--- backend/inventoryb/src/main/java/com/t27/inventoryapp/repository/RoleRepository.java
+++ backend/inventoryb/src/main/java/com/t27/inventoryapp/repository/RoleRepository.java
@@ -1,6 +1,6 @@
 package com.t27.inventoryapp.repository;
 
-import java.util.Optional;
+import java.util.Optional; 
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- backend/inventoryb/src/main/java/com/t27/inventoryapp/security/WebSecurityConfig.java
+++ backend/inventoryb/src/main/java/com/t27/inventoryapp/security/WebSecurityConfig.java
@@ -1,5 +1,36 @@
 package com.t27.inventoryapp.security;
 
+import com.t27.inventoryapp.security.jwt.AuthTokenFilter;
+import com.t27.inventoryapp.security.services.*;
+import com.t27.inventoryapp.security.jwt.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class WebSecurityConfig {
-    
+
+    @Autowired
+     UserDetailsServiceImpl userDetailService;
+
+    @Autowired
+    private AuthEntryPointJwt unathorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+     
+        return authProvider;
+    }
 }
