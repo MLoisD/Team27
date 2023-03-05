ee32c49581ea0494cdb6e17bc0a28477a9600cb6
MLoisD <davidssinmi@gmail.com>
2023-03-05 19:06:49 +0000

    security config

--- backend/inventoryb/src/main/java/com/t27/inventoryapp/security/jwt/JwtUtils.java
+++ backend/inventoryb/src/main/java/com/t27/inventoryapp/security/jwt/JwtUtils.java
@@ -1,5 +1,16 @@
 package com.t27.inventoryapp.security.jwt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+
 public class JwtUtils {
-    
+   
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${t27.inventoryb.jwtSecret}")
+    private String jwtSecret;
+
+    //@Value("${t27.inventoryb.jwtExpirationMs}")
 }
