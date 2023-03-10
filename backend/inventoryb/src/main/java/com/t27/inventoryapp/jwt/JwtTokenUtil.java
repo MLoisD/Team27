4759d04f232dc6f50fda6b4ed48b26f8199175f7
MLoisD <davidssinmi@gmail.com>
2023-03-10 21:49:31 +0000

    adding JWT token

--- backend/inventoryb/src/main/java/com/t27/inventoryapp/jwt/JwtTokenUtil.java
+++ backend/inventoryb/src/main/java/com/t27/inventoryapp/jwt/JwtTokenUtil.java
@@ -1,5 +1,25 @@
 package com.t27.inventoryapp.jwt;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.t27.inventoryapp.model.User;
+import java.util.Date;
+ 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
 public class JwtTokenUtil {
+    private static final long EXPIRE_DURATION = 24 * 60 * 60 * 1000;
     
+    @Value("${app.jwt.secret}")
+    private String SECRET_KEY;
+
+    public String generateAccessToken(User user){
+        return Jwts.builder().setSubject(String.format("%s,%s", user.getId(), user.getUseremail()))
+                .setIssuer("T27").setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis() + EXPIRE_DURATION))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+    }
 }
