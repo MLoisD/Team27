a3ec9114f5041a9045a8dde4b131b26adfcef690
MLoisD <davidssinmi@gmail.com>
2023-03-12 22:08:08 +0000

    update troubleshoot starting

--- backend/inventoryb/src/main/java/com/t27/inventoryapp/InventorybApplication.java
+++ backend/inventoryb/src/main/java/com/t27/inventoryapp/InventorybApplication.java
@@ -2,11 +2,12 @@ package com.t27.inventoryapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-public class InventorybApplication {
+public class InventorybApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventorybApplication.class, args);
