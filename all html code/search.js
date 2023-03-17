2221c0a42f534fc9ac74265198c45aad664a5fa8
Sijan Hossain <210049864@aston.ac.uk>
2023-03-17 19:03:31 +0000

    Search bar working.

--- all html code/search.js
+++ all html code/search.js
@@ -1,58 +1,31 @@
-// const searchInput = document.getElementById('search_item');
-// searchInput.addEventListener('input', searchItems);
 
-// const message = document.createElement('div');
-// message.textContent = 'No items found';
-// message.style.display = 'none';
-// document.querySelector('.product_list').appendChild(message);
-
-// function searchItems() {
-//   const searchQuery = searchInput.value.toLowerCase();
-//   const items = document.querySelectorAll('.card');
-//   let matchFound = false;
-  
-//   items.forEach(item => {
-//     const title = item.querySelector('.title').textContent.toLowerCase();
-//     const author = item.querySelector('.author_name').textContent.toLowerCase();
+function search() {
+    // Get the user input from the search bar
+    const input = document.getElementById("search_item").value.toLowerCase();
     
-//     if(title.includes(searchQuery) || author.includes(searchQuery)) {
-//       item.style.display = 'block';
-//       matchFound = true;
-//     } else {
-//       item.style.display = 'none';
-//     }
-//   });
-  
-//   if(matchFound) {
-//     message.style.display = 'none';
-//   } else {
-//     message.style.display = 'block';
-//   }
-// }
-
-
-
-
-
-const search = () =>{
-    const searchbox = document.getElementById("search_item").value.toUpperCase();
-    const storeitems = document.getElementById("product_container")
-    const product = document.querySelectorAll (".card")
-    const pname = storeitems.getElementsByTagName ("h2")
-
-    for (var i = 0; i < pname.length; i++){
-         let match = product [i].getElementsByTagName('h2')[0];
-        if (match){
-        let textvalue = match. textContent || match.innerHTML
-
-        if (textvalue.toUpperCase().indexOf (searchbox) > -1) {
-        product[i].style.display = "";
-        }else {
-       product[i].style.display = "none";
+    // Get all product cards on the page
+    const cards = document.querySelectorAll(".card");
+    
+    // Loop through each card and check if the title contains the search input
+    for (let i = 0; i < cards.length; i++) {
+      const title = cards[i].querySelector(".title").textContent.toLowerCase();
+      const author = cards[i].querySelector(".author_name").textContent.toLowerCase();
+    
+      // If the title contains the search input, show the card; otherwise, hide the card
+      if (title.includes(input) || author.includes(input))  {
+        cards[i].style.display = "block";
+      } else {
+        cards[i].style.display = "none";
+      }
     }
-}
+  }
+
+  const searchBar = document.getElementById("search_item");
+  searchBar.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      search();
     }
-}
+  });
 
     
 
