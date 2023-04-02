9881f016727c3d8508c86ecff32714d2bae0d6d8
BernardMithi <210093454@aston.ac.uk>
2023-04-02 21:29:23 +0100

    Refactored adminlayout routes

--- frontendweb/src/AdminLayoutRoutes.js
+++ frontendweb/src/AdminLayoutRoutes.js
@@ -19,29 +19,26 @@ function App() {
 
   return (
     <div className={darkMode ? "app dark" : "app"}>
-      <React.Fragment>
+      <>
         <Routes>
-          <Route path="" element={<Home />} />
-          <Route path="user" element={<User />} />
-          <Route path="summary" element={<OrderSummary />} />
-          <Route path="users">
-            <Route index element={<Users />} />
-            <Route path=":userId" element={<Single />} />
-          </Route>
-          <Route path="books" element={<Booklist />} />
-          <Route path="books/:bookId" element={<BookSingle />} />
-          <Route
-            path="books/:bookId/edit/:id"
-            element={<Edit inputs={productInputs} title="EDIT BOOK" />}
-          />
-          <Route path="orders">
-            <Route index element={<Orders />} />
-          </Route>
-          <Route path="new">
-            <Route index element={<New inputs={productInputs} />} />
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="user" element={<User />} />
+            <Route path="users">
+              <Route index element={<Users />} />
+              <Route path=":userId" element={<Single />} />
+            </Route>
+            <Route exact path="books" element={<Booklist />}/>
+            <Route exact path="books/:bookId" element={<BookSingle/>} />
+            <Route exact path="books/:bookId/edit/:id" element={<Edit inputs={productInputs} title="EDIT BOOK" />}/>
+            <Route exact path="books/new" element={<New inputs={productInputs} /> } />
+            <Route path="orders">
+              <Route index element={<Orders/>} />
+              <Route path="summary" element={<OrderSummary />} />
+            </Route>
           </Route>
         </Routes>
-      </React.Fragment>
+      </>
     </div>
   );
 }
